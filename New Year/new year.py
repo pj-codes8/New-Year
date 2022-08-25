b45dd309a87e4884cf0c5c702d7a0c499a1071c5
codeDivine8 <77944180+codeDivine8@users.noreply.github.com>
2022-08-24 21:14:29 -0700

    Add files via upload

---
+++ New Year/new year.py
@@ -0,0 +1,14 @@
+# Happy New Year Program
+
+def new_year(year):
+  countdown = [10,9,8,7,6,5,4,3,2,1]
+
+  for count in countdown:
+    print(count)
+
+  if True:
+    print("Happy New Year!!!")
+    print("It is " + str(year) +"!!!")
+
+
+# new_year(# Any year goes in this argument) # Outputs the new year function
